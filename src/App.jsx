fd9146bd97c7c9816080e2ff6dece3605e764c80
Akash S Devadiga <4mt24is012@mite.ac.in>
2026-07-23 19:21:32 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AA from  "./components/AA"
+
+
 
 function App() {
-  return <h1>Hello World</h1>
+
+  const[counter,setCounter]=useState(0)
+
+  return <><h1>Hello World</h1>
+  <button onClick={()=>setCounter(prev=>prev+1)}>Click Here</button>
+  <h1>{counter}</h1>
+
+  </>
 }
 
 export default App
---
+++ src/components/AA.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AA() {
+  return (
+    <div>
+      helloooo buddies i am in AA.jsx
+    </div>
+  )
+}
+
+export default AA
